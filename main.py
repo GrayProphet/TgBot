9ab7f3b8b4c8a067881fae8607f93fa04eae890e
GrayProphet <bygrayprophet@gmail.com>
2023-01-14 13:17:28 +0300

    fix

--- main.py
+++ main.py
@@ -11,16 +11,38 @@ dp = Dispatcher(bot)
 HELP_MESSAGE =  """
 <b>/help</b> - <em>список команд</em>
 <b>/start</b> - <em>старт бота</em>
-<b>/bay</b> - <em>купить рыбов</em>
+<b>/buy</b> - <em>купить рыбов</em>
 """
+
+
 @dp.message_handler(commands=['help'])
 async def send_help_command(message: types.Message):
-    await bot.send_message(message.chat.id, HELP_MESSAGE,"HTML")
+    await bot.send_message(message.chat.id, HELP_MESSAGE, "HTML")
 
 
-@dp.message_handler(commands=['bay'])
+@dp.message_handler(commands=['buy'])
 async def send_bay_command(message: types.Message):
-    await bot.send_message(message.chat.id, f"Сейчас есть {len(ribov_list)} рыбов в наличии. Сколько рыбов вы хотитете?")
+    args = message.get_args()
+    if len(ribov_list) == 0:
+        return await bot.send_message(message.chat.id,
+                                      "Мы сожалеем, но рыбов не осталось, приходите по позже")
+    if not args:
+        await bot.send_message(message.chat.id,
+                               f"Сейчас есть {len(ribov_list)} рыбов в наличии. Сколько рыбов вы хотитете?")
+    else:
+        if args.isdigit():
+            args = int(args)
+            if args > len(ribov_list):
+                return await bot.send_message(message.chat.id,
+                                              "Мы сожалеем, но рыбов не хватит поробуйте ввести число поменьше")
+            else:
+                for i in range(args):
+                    print(i)
+                    ribov_list.pop()
+                await bot.send_message(message.chat.id, f"Осталось {len(ribov_list)} рыбов в наличии.")
+        else:
+            return await bot.send_message(message.chat.id, "Ой кажется вы ввели не верное значение, введите пожалуйста цифру")
+
 
 @dp.message_handler(commands=['start'])
 async def send_welcome_command(message: types.Message):
